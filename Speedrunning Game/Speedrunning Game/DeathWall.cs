dae2bb85e9dcc52dd92d7b57722defb7175efa0b
Tad Cordle <jellyfish646@aim.com>
2012-06-26 15:30:07 -0400

    Added some death wall tiles (graphics most likely temporary)

---
+++ Speedrunning Game/Speedrunning Game/DeathWall.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using Game_Maker_Library;
+
+namespace Speedrunning_Game
+{
+	class DeathWall : Wall
+	{
+		public DeathWall(Rectangle r) 
+			: base (r)
+		{
+		}
+
+		public DeathWall(int x, int y, int w, int h)
+			: base(x, y, w, h)
+		{
+		}
+	}
+}
